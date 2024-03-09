c806446b3c35abe57dae64f753e62799de46289a
Rijikashi <isaacjfu@gmail.com>
2024-03-09 14:40:22 -0800

    connecting to yahoo servers for league data

--- src/main.py
+++ src/main.py
@@ -1,7 +1,61 @@
-from flask import Flask
+from flask import Flask, request
+from dotenv import load_dotenv
+import os
+import requests
+import webbrowser
+
+load_dotenv()
+
+client_id = os.getenv('CLIENT_ID')
+client_secret = os.getenv('CLIENT_SECRET')
+league_id = os.getenv('LEAGUE_ID')
+redirect_uri = 'https://redirectmeto.com/http://localhost:5000/redirect'
+url_base = 'localhost:5000'
+
+def request_handler():
+    
+    u = f'https://api.login.yahoo.com/oauth2/request_auth?'
+    p = {
+        'client_id': client_id,
+        'redirect_uri': redirect_uri,
+        'response_type' : 'code'
+    }
+    print(u)
+    r = requests.get(u,p)
+    webbrowser.open(r.url)
+
 
 app = Flask(__name__)
-
 @app.route("/")
 def hello_world():
+    request_handler()
     return "hi"
+
+@app.route("/redirect", methods = ['GET', 'POST'])
+def redirect():
+    code = request.args.get('code')
+    u = f"https://api.login.yahoo.com/oauth2/get_token?"
+    p = {
+        'client_id' : client_id,
+        'client_secret' : client_secret,
+        'redirect_uri': redirect_uri,
+        'code': code,
+        'grant_type': 'authorization_code'
+    }
+    r = requests.post(u,p)
+    response = r.json()
+    print(response)
+    token = response['access_token']
+    u = f'{url_base}/controller?token={token}'
+    print(u)
+    print(token)
+    webbrowser.open(u)
+    return "redirect"
+
+@app.route('/controller', methods = ['GET','POST'])
+def controller():
+    token = request.args.get('token')
+    l_id = 'nba.l.{league_id}'
+    print(token)
+    print(l_id)
+    return "controller"
