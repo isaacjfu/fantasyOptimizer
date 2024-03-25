36714555e20fd6134d08d32826220e645e78eb95
Rijikashi <isaacjfu@gmail.com>
2024-03-25 15:54:17 -0700

    finished all data retrieval of player stats and schedule, league rosters, free agents, team matchups

--- src/main.py
+++ src/main.py
@@ -16,7 +16,8 @@ client_secret = os.getenv('CLIENT_SECRET')
 league_id = os.getenv('LEAGUE_ID')
 redirect_uri = 'https://redirectmeto.com/http://localhost:5000/redirect'
 url_base = 'localhost:5000'
-access_token =  'pSKWG6qYpguiJ3e_HEEy5hoUUmf7QonctrTLL5xYXIxX3Pui3CeBYARd8BzDFHDyvsiCUBcdQZMpcInsE.vI8NIKg6ZNrZY0vh3OOomI7pz6lCrU4GGQrjZ3chdsgBWf7Cb3CxEZDn32UD4pCbS95UH_j0e6IwkU6pAQDeNGAN2xqEUu9kWNo6hT1wiajuoJ.uwkRGCd9EOF.aK9JyunNT4qht2Anjf0WzT86Z1B33Z3Ih5KlzcccAaMkqzgjwFxbYQhDksYNUZyHlf3q2BNV5fXFzDkFsLDtQJt6r.22qHymTmW3pi_IbbZdFwmBQrgRT6TkEwII72vtvTiH2PTazeaVNRaP3POg_FedLY6EZzCrQChHkny8x5Y0MmQ6fp_X.8dzrYuwea3x4tyW4BeTgQ1UealD6XE.oGp9EZ8AoEHTVhBCFbQW2eg8OI3H4iH9M_j.9AtbT8A50NZggEyX279FCnJ_KuvcKuGia2sDys.HcOe7u4SyTEaw4sIYWRMnEXunsmK3n8N0WeJaf9GdCxnL7fIANFEj1CmdUsnab.rFqdOv_cQKyCCfz5mDZofqor7HLdJtHJDokiPksIhifinOkwQ_O3hm2_ohSmnkDNmZ3J7inuN2fqoWw8_08Oq86l07LJA9J_NSUEYLDyHtfapUZ6QvBjrbCCxuMsnYjIkPJogUyeEYeQeeK5IdeXnSZCSmNRDLpoH3ZfKryIfGeEG.swmavtj5zb_HfD0U8znJ5shlZc84En86jvjjlclZ9Nb9EdKUMnunybHO2w_YEFLYkRwU33rquZRebNBxQTH26I27pIo0tIX7efhkJ1qM3rrSo6h_bPPwt0hu.tnRUYEhxqgkDL51dupEH2VLwLkwavegPz8b3x1BA2N3tnU3j7okg7Ld4aFxzGzn9ZLDpZsZhNUtjn8'
+access_token =  'Rbe387qYpgsLbWTT_MjFdPtFYB8buY4EDQvQ68f15GBaxk6g.RVbQ.P6mdT60LUvIK2xBIKARv1rQ4Z.x8IhEUSAvoNJpnFHe5uJolaGvHt_5vDMuk4MWTZcUdIemFb_DwbenkwRn7izkTuLaS.480Qdj8h8o7DNlL6ifbRCbFWGBhWhF6A2sspn3ZATusyGEiP5p9t4MHMMCb74mkCq8MB0Nlg5DKzJ6f6ZHpfsXBUetHDMhnwnijr9neadoA7o1VJPJan5A3hTCY0hyu1UuMxG7OzBoPYC8pnrPScaJ6cDYR88Kv83wuq_tTEv9VtVW4eGK1eZlqeHESlnxDS6eZgb_yr5dweLatGnkn7ynLlT8Tj59akdzetuiXg_2LZTyFP.nWl6ZwbUtgesvsBHs9B07vzV7DHneSqzOuBofZVVpVUCeA5An8NfUCHnfZIIMBGmuVSglOcTs4fAqDtji7xcVkVfiNJcR6QNUtETFLhU3O_.O56dmpofvF22RUMhoRtHlaH7GH7ns1pPl3XSzouKX0sdPoWmXpv_g6BaGuEYUuuI507Rhbt8ULS91_VNY1194yKiTYRUyDyOyQ0iak5_N3EUXWO1QnuMXnFI4F4fa3.2AkyugBl1uJJP82cF0HutF2IkcL2_ea34gLeG2PvrFTfC3_593FCXYw2vqi.sy4p5D9lA.TMNF1QYAzQyQ61iP7ibcmWQ5ILAPMvoAkzKFWNzY84fu46MEi8S3KmfHhcYnOpAzbq4AOmyG5HONHqkgByZL08vreKDXPMiCdx86jFkPzRCun2DQFMcxsBw09IL5iy.Z3CEdHtL0X5p309LbwapcvXaR6f.x5wz27CCJm73_o28Wtsj8nWj8Ku5AWkRHAa2EzLVrzorJJYg2RdaBGwcBgHYC4adlOQTxKY_ec.v7WLB'
+
 CONST_CURRENT_SEASON = '2023-24'
 # may need to refactor into the settings query
 CONST_STAT_IDENTIFER = {
@@ -82,9 +83,9 @@ def query(token, l_id):
     }
 
     #settings = settings_query(h,l_id)
-    players = players_query(h,l_id)
+    #players = players_query(h,l_id)
     #team = team_query(h,l_id) 
-    #matchup = matchup_query(h,l_id)
+    matchup = matchup_query(h,l_id)
 
     return "asdf"
 
@@ -151,7 +152,6 @@ def team_query(h,l_id):
             jersey_number = t_elem.find('uniform_number').text
 
             player_key = f"{full_name}/{team_name}/{jersey_number}"
-
             teams[key]['roster'].append(player_key)
     print(teams)
 
@@ -164,6 +164,8 @@ def players_query(h,l_id):
     print(players) 
 
 def players_query_helper(h,l_id,playersDictionary,status):
+    f = open('playerStats.json')
+    player_stats = json.load(f)
     start_idx = 0
     end = False
     while(not end):
@@ -204,49 +206,69 @@ def players_query_helper(h,l_id,playersDictionary,status):
             playersDictionary[player_key]['pos'] = []
             for pos in eligible_node.findall('position'):
                 playersDictionary[player_key]['pos'].append(pos.text)
-            player_stats_node = player.find('player_stats')
-            stats_node = player_stats_node.find('stats')
+            
+            # stats
+            # potentially need to filter only out the stats that i need. when im adding compatibility with different league settings
+            playersDictionary[player_key]['stats'] = player_stats[player_key]['stats'] if player_stats.get(player_key) != None else []
+
+            #schedule
+            playersDictionary[player_key]['schedule'] = player_stats[player_key]['schedule'] if player_stats.get(player_key) != None else []
     
 def matchup_query(h,l_id):
-    team_1 = {
-        'cat_totals': [],
-        'roster': []
+    matchups_dict = {
+        'start' : '',
+        'end' : '',
+        'matchups': []
     }
-    team_2 = {
-        'cat_totals': [],
-        'roster' : []
-    }
+
     u = f'https://fantasysports.yahooapis.com/fantasy/v2/league/{l_id}/scoreboard'
     r = requests.get(u,headers = h)
     encoded = r.text.encode(encoding = 'ascii', errors = 'ignore').decode("utf-8")
-
-    f = open("test.txt","w")
-    f.write(encoded)
-    f.close()
     root = strip_namespace(encoded)
     
-    print(ET.tostring(root).decode())
-    for elem in root.iter('matchup'):
-        print(elem)
-
-    print("hello")
+    for matchups in root.iter('matchups'):
+        for matchup in matchups.findall('matchup'):
+            matchups_dict['start'] = matchup.find('week_start').text
+            matchups_dict['end'] = matchup.find('week_end').text
+            teams_node = matchup.find('teams')
+            matchup = {
+                'team_1' : '',
+                'team_2': '',
+                'team_1_stats' : [],
+                'team_2_stats' : []
+            }
+            count = 1
+            for team in teams_node.findall('team'):
+                team_str = f'team_{count}'
+                team_stats_str = f'team_{count}_stats'
+                matchup[team_str] = team.find('team_key').text
+                team_stats = team.find('team_stats')
+                stats_node = team_stats.find('stats')
+                for stat in stats_node.findall('stat'):
+                    stat_id = stat.find('stat_id').text
+                    if CONST_STAT_IDENTIFER.get(stat_id) != None:
+                        matchup[team_stats_str].append(stat.find('value').text)
+                count += 1
+                
+            matchups_dict['matchups'].append(matchup)
+    print(matchups_dict)
+    return matchups_dict
+    # f = open("test.txt","w")
+    # f.write(encoded)
+    # f.close()
 
 def import_player_data():
     #print(all_players)
     #for i in range(0,len(all_players)):
     player_dict = {}
+    f = open('teamSchedules.json')
+    teamSchedules = json.load(f)
     for i in range(0,len(all_players)):
         player = all_players[i]
         career_stats = playercareerstats.PlayerCareerStats(per_mode36 = "PerGame", player_id = player['id']).get_json()
         player_info = commonplayerinfo.CommonPlayerInfo(player_id = player['id']).get_json()
-        schedule_info = playernextngames.PlayerNextNGames(number_of_games = '82', player_id = player['id'] , season_all = CONST_CURRENT_SEASON, season_type_all_star = 'Regular Season').get_json()
         career_obj = json.loads(career_stats)
         player_obj = json.loads(player_info)
-        schedule_info = json.loads(schedule_info)
-        a = schedule_info['resultSets'][0]['rowSet']
-        schedule = []
-        for i in a:
-            schedule.append(i[1])
         index = 0
         for i in range(len(career_obj["resultSets"])):
             if career_obj["resultSets"][i]["name"] == "SeasonTotalsRegularSeason":
@@ -255,7 +277,6 @@ def import_player_data():
         player_info = player_obj['resultSets'][0]['rowSet'][0]
         player_key = f'{player_info[3]}/{player_info[20]}/{player_info[14]}'
         if len(career_obj['resultSets'][index]['rowSet']) != 0:
-
             season_stats = career_obj["resultSets"][index]["rowSet"][-1]
             if season_stats[1] == CONST_CURRENT_SEASON:
                 filtered_stats = {
@@ -291,7 +312,7 @@ def import_player_data():
                 }
             player_dict[player_key] = {
                 'stats' : filtered_stats,
-                'schedule' : schedule
+                'schedule' : teamSchedules[player_obj['resultSets'][0]['rowSet'][0][20]] if player_obj['resultSets'][0]['rowSet'][0][20] != '' else {}
             }
         print(player_key)
     print(len(player_dict))
@@ -299,16 +320,32 @@ def import_player_data():
     with open('playerStats.json', "w") as outfile:
         json.dump(player_dict,outfile,indent = 2)
 
+def import_team_data():
+    team_schedules = {
+
+    }
+    r = requests.get("https://cdn.nba.com/static/json/staticData/scheduleLeagueV2.json").json()
+    games = r['leagueSchedule']['gameDates']
+
+    for date in games:
+        print(date['gameDate'])
+        for game in date['games']:
+            print(game['gameLabel'])
+            if (game['gameLabel'] == ''):
+                if team_schedules.get(game['homeTeam']['teamTricode']) == None:
+                    team_schedules[game['homeTeam']['teamTricode']] = [date['gameDate']]
+                else:
+                    team_schedules[game['homeTeam']['teamTricode']].append(date['gameDate'])
+                if team_schedules.get(game['awayTeam']['teamTricode']) == None:
+                    team_schedules[game['awayTeam']['teamTricode']] = [date['gameDate']]
+                else:
+                    team_schedules[game['awayTeam']['teamTricode']].append(date['gameDate'])
+    with open('teamSchedules.json', "w") as outfile:
+        json.dump(team_schedules,outfile,indent = 2)
+
 def test_function():
-    x = cumestatsteamgames.CumeStatsTeamGames( league_id = '00', season = '2023-24', season_type_all_star = 'Regular Season', team_id = '1610612739').get_json()
-    y = playernextngames.PlayerNextNGames(number_of_games = '100', player_id = '203999', season_all = '2023-24', season_type_all_star = 'Regular Season').get_json()
-    asdf = json.loads(y)
-    print(asdf['resultSets'][0]['rowSet'])
-    a = asdf['resultSets'][0]['rowSet']
-    s = []
-    for i in a:
-        s.append(i[1])
-    print(s)
+    pass
 
 #test_function()
-import_player_data()
+#import_team_data() 
+#import_player_data()
